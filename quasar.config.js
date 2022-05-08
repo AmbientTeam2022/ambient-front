00bb20148af55abd289aaef48770b01761f63e78
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2022-05-08 15:03:40 -0400

    agrega login ejemplo

--- quasar.config.js
+++ quasar.config.js
@@ -8,9 +8,9 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js
 
-const ESLintPlugin = require("eslint-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin')
 
-const { configure } = require("quasar/wrappers");
+const { configure } = require('quasar/wrappers')
 
 module.exports = configure(function (ctx) {
   return {
@@ -23,10 +23,10 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
-    boot: ["axios"],
+    boot: ['axios'],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
-    css: ["app.scss"],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -38,13 +38,13 @@ module.exports = configure(function (ctx) {
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      "roboto-font", // optional, you are not bound to it
-      "material-icons", // optional, you are not bound to it
+      'roboto-font', // optional, you are not bound to it
+      'material-icons', // optional, you are not bound to it
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: "hash", // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -68,21 +68,21 @@ module.exports = configure(function (ctx) {
 
       chainWebpack(chain) {
         chain
-          .plugin("eslint-webpack-plugin")
-          .use(ESLintPlugin, [{ extensions: ["js", "vue"] }]);
+          .plugin('eslint-webpack-plugin')
+          .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }])
         chain.module
-          .rule("pug")
+          .rule('pug')
           .test(/\.pug$/)
-          .use("pug-plain-loader")
-          .loader("pug-plain-loader")
-          .end();
+          .use('pug-plain-loader')
+          .loader('pug-plain-loader')
+          .end()
       },
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
     devServer: {
       server: {
-        type: "http",
+        type: 'http',
       },
       port: 8080,
       open: true, // opens browser window automatically
@@ -104,6 +104,7 @@ module.exports = configure(function (ctx) {
 
       // Quasar plugins
       plugins: [],
+      removeDefaultCss: true,
     },
 
     // animations: 'all', // --- includes all animations
@@ -125,19 +126,19 @@ module.exports = configure(function (ctx) {
 
       chainWebpackWebserver(chain) {
         chain
-          .plugin("eslint-webpack-plugin")
-          .use(ESLintPlugin, [{ extensions: ["js"] }]);
+          .plugin('eslint-webpack-plugin')
+          .use(ESLintPlugin, [{ extensions: ['js'] }])
       },
 
       middlewares: [
-        ctx.prod ? "compression" : "",
-        "render", // keep this as last one
+        ctx.prod ? 'compression' : '',
+        'render', // keep this as last one
       ],
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
@@ -145,43 +146,43 @@ module.exports = configure(function (ctx) {
 
       chainWebpackCustomSW(chain) {
         chain
-          .plugin("eslint-webpack-plugin")
-          .use(ESLintPlugin, [{ extensions: ["js"] }]);
+          .plugin('eslint-webpack-plugin')
+          .use(ESLintPlugin, [{ extensions: ['js'] }])
       },
 
       manifest: {
         name: `Ambient`,
         short_name: `Ambient`,
         description: `Frontend del proyecto Ambient`,
-        display: "standalone",
-        orientation: "portrait",
-        background_color: "#ffffff",
-        theme_color: "#027be3",
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#027be3',
         icons: [
           {
-            src: "icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
           },
           {
-            src: "icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
+            src: 'icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
           },
           {
-            src: "icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
           },
           {
-            src: "icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
@@ -199,7 +200,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-electron-apps/configuring-electron
     electron: {
-      bundler: "packager", // 'packager' or 'builder'
+      bundler: 'packager', // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
@@ -215,22 +216,22 @@ module.exports = configure(function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "ambient-front",
+        appId: 'ambient-front',
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
 
       chainWebpackMain(chain) {
         chain
-          .plugin("eslint-webpack-plugin")
-          .use(ESLintPlugin, [{ extensions: ["js"] }]);
+          .plugin('eslint-webpack-plugin')
+          .use(ESLintPlugin, [{ extensions: ['js'] }])
       },
 
       chainWebpackPreload(chain) {
         chain
-          .plugin("eslint-webpack-plugin")
-          .use(ESLintPlugin, [{ extensions: ["js"] }]);
+          .plugin('eslint-webpack-plugin')
+          .use(ESLintPlugin, [{ extensions: ['js'] }])
       },
     },
-  };
-});
+  }
+})
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,12 +1,27 @@
 <template lang="pug">
+//- .q-pa-lg
+//-   q-input(v-model.number='cantidad' type='number' filled rounded).input-cantidad
+.column.justify-between.cuadro.q-ml-lg
+  h1 Ambient
+  .q-mx-md
+    q-input( v-model="usuario" placeholder="hola" )
+  p.q-ma-none esto es un footer
 
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      usuario: '',
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+.cuadro {
+  height: 100vh;
+  width: 400px;
+}
 </style>
